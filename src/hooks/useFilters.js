7bb5291fb1d49b44a6126a48aa015ac2625ed075
osimjon <osimjon90@yandex.ru>
2024-07-20 21:03:41 +0300

    fix header BackgroundColor

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export const useFilters = (initialFilters) => {
   const [filters, setFilters] = useState(initialFilters);
